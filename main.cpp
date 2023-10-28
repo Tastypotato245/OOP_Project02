6a7b90c791792a6bf42dbb2f5c8ca3fb29ef61ba
Tastypotato245 <rbt2775@cau.ac.kr>
2023-10-28 20:47:06 +0900

    comp: but some errors in sub, minus number plus or minus

--- main.cpp
+++ main.cpp
@@ -11,8 +11,8 @@ int	op_switching(inf_int a, char op, inf_int b)
 	else if (op == '*')
 		cout << "Output: " << a * b << "\n";
 	else
-		return (0);
-	return (-1);
+		return (-1);
+	return (0);
 }
 
 int	read_process()
@@ -43,7 +43,6 @@ int	argv_process(char **argv)
 
 	if (strlen(argv[2]) != 1)
 		return (-1);
-	std::cout << "test" ;
 	return (op_switching(a, op, b));
 }
 
@@ -57,42 +56,3 @@ int main(int argc, char **argv)
 		return (0);
 	return (-1);
 }
-
-/*
-	inf_int a, e;
-	inf_int b(100);
-	inf_int c("123");
-	inf_int d("12345");
-	inf_int f = d;
-	inf_int g(f);
-
-	cout << "a : " << a << "\n";
-	cout << "b : " << b << "\n";
-	cout << "c : " << c << "\n";
-	cout << "d : " << d << "\n";
-	cout << "e : " << e << "\n";
-
-	cout << "b : " << b << "\n";
-	cout << "c : " << c << "\n";
-	cout << "=========================" << "\n";
-	cout << "b * c : " <<  b * c << "\n";
-	a = b * c;
-	cout << "a : " << a << "\n";
-	a = a * c;
-	cout << "a : " << a << "\n";
-	a = a * c;
-	cout << "a : " << a << "\n";
-	cout << "=========================" << "\n";
-	cout << "+++++++++++++++++++++++++" << "\n";
-	a = b + c;
-	cout << "a : " << a << "\n";
-	b = c - d;
-
-	if (f == d) {
-		cout << "a : " << a << "\n";
-		cout << "b : " << b << "\n";
-		cout << "c : " << c << "\n";
-		cout << "d : " << d << "\n";
-		cout << "f : " << f << "\n";
-	}
-*/
